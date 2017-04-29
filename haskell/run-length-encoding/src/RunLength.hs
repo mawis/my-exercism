2058604cce67586b462e8bb06fa2e5dc8a4c49e7
Matthias Wimmer <m@tthias.eu>
2017-04-29 23:32:03 +0200

    2nd iteration: Run Length Encoding @Haskell
    * Code cleanup
    * explicitly defining that `asPairs` needs to be called on an even sized
      list

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -12,16 +12,17 @@ decode encodedText
 asPairs :: [String] -> [(String, String)]
 asPairs [] = []
 asPairs (len : grp : xs) = (len, grp) : asPairs xs
+asPairs _ = error "Even nummer of elements expected."
 
 grpNumber :: Char -> Char -> Bool
 grpNumber a b = isNumber a == isNumber b
 
 decodePair :: (String, String) -> String -> String
-decodePair (len, grp) accu = take (read len) (repeat $ head grp) ++ tail grp ++ accu
+decodePair (len, grp) accu = replicate (read len) (head grp) ++ tail grp ++ accu
 
 encode :: String -> String
 encode = foldr prependGroup "" . group
 
 prependGroup :: String -> String -> String
 prependGroup [c] accu = c : accu
-prependGroup grp accu = (show $ length grp) ++ (head grp : accu)
+prependGroup grp accu = show (length grp) ++ (head grp : accu)
