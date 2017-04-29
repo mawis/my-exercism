e6d9098ad6a5f57a3e699741d8765ddced39520e
Matthias Wimmer <m@tthias.eu>
2017-04-29 21:16:11 +0200

    5th iteration: Hamming @Haskell
    Code cleanup.

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -2,5 +2,5 @@ module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
 distance [] [] = Just 0
-distance (x:xs) (y:ya) = fmap (+ (fromEnum $ x /= y)) $ distance xs ya
+distance (x:xs) (y:ya) = (+ (fromEnum $ x /= y)) <$> distance xs ya
 distance _ _ = Nothing
