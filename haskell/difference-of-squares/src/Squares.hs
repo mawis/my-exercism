dd54a2935f0cbf6940b2e4fd1a62e1cc27cd9d4c
Matthias Wimmer <m@tthias.eu>
2017-04-29 21:02:36 +0200

    2nd iteration: Difference Of Squares @Haskell
    Added type hint. Without it doesn't compile in pedantic mode.

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -4,7 +4,7 @@ difference :: Integral a => a -> a
 difference n = squareOfSums n - sumOfSquares n
 
 squareOfSums :: Integral a => a -> a
-squareOfSums n = sum [1..n] ^ 2
+squareOfSums n = sum [1..n] ^ (2 :: Integer)
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = sum (map (^2) [1..n])
+sumOfSquares n = sum (map (^ (2 :: Integer)) [1..n])
