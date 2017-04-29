af2c684602ec23ba340879bd4356c2209648b246
Matthias Wimmer <m@tthias.eu>
2017-04-29 21:24:48 +0200

    3rd iteration: Nucleotide Count @Haskell
    Code cleanup.

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -6,6 +6,6 @@ nucleotideCounts :: String -> Either String (Map Char Int)
 nucleotideCounts "" = Right
                       $ fromList [ ('A', 0), ('C', 0), ('G', 0), ('T', 0) ]
 nucleotideCounts (x:xs)
-    | x `elem` "ACGT" = fmap (oneMore x) $ nucleotideCounts xs
+    | x `elem` "ACGT" = oneMore x <$> nucleotideCounts xs
     | otherwise       = Left "Invalid nucleotide"
   where oneMore = adjust (+1)
