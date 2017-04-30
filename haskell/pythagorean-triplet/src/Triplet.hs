c6bc3ffcf6ec8e4674b21243edaba1aed9b00341
Matthias Wimmer <m@tthias.eu>
2017-04-30 22:20:34 +0200

    2nd iteration: Pythagoran Triplet @Haskell
    Using `Control.Monad.guard` instead of `filter`.

--- haskell/pythagorean-triplet/src/Triplet.hs
+++ haskell/pythagorean-triplet/src/Triplet.hs
@@ -1,5 +1,6 @@
 module Triplet (isPythagorean, mkTriplet, pythagoreanTriplets) where
 
+import Control.Monad (guard)
 import Data.List (sort)
 
 isPythagorean :: (Int, Int, Int) -> Bool
@@ -11,9 +12,10 @@ mkTriplet :: Int -> Int -> Int -> (Int, Int, Int)
 mkTriplet a b c = (a, b, c)
 
 pythagoreanTriplets :: Int -> Int -> [(Int, Int, Int)]
-pythagoreanTriplets minf maxf = filter isPythagorean candidates
-  where candidates = do
-          a <- [minf..maxf - 2]
-          b <- [a + 1..maxf - 1]
-          c <- [b + 1..maxf]
-          return (a, b, c)
+pythagoreanTriplets minf maxf =  do
+  a <- [minf..maxf - 2]
+  b <- [a + 1..maxf - 1]
+  c <- [b + 1..maxf]
+  let abc = mkTriplet a b c
+  guard (isPythagorean abc)
+  return abc
