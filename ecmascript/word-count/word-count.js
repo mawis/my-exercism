2c82f4813562962b9529cac42da2d5f2134ccb42
Matthias Wimmer <m@tthias.eu>
2017-05-01 01:02:46 +0200

    2nd iteration: Word Count @ECMAScript
    * Array update taken from
      http://exercism.io/submissions/9e2cd23598464d939c225b8e8b57169c
    * Cleanup RegEx to split into words to use a complete character class.

--- ecmascript/word-count/word-count.js
+++ ecmascript/word-count/word-count.js
@@ -1,12 +1,12 @@
 const addWords = (accu, word) => {
-  accu[word] = accu.hasOwnProperty(word) ? accu[word] + 1 : 1;
+  accu[word]++ || (accu[word] = 1);
   return accu
 }
 
 export default class Words {
   count(text) {
     return text.trim()
-      .split(/[ \n\t]+/)
+      .split(/\s+/)
       .map(s => s.toLowerCase())
       .reduce(addWords, {})
   }
