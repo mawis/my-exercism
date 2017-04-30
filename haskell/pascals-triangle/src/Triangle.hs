78a38aef97f5e0601e17b203facf6a93a8365491
Matthias Wimmer <m@tthias.eu>
2017-04-30 21:45:33 +0200

    2nd iteration: Pascal's triangle @Haskell
    Variation of my first submission.

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -4,9 +4,5 @@ rows :: Int -> [[Integer]]
 rows x
   | x < 1 = []
   | otherwise = take x triangle
-
-triangle :: [[Integer]]
-triangle = iterate nextRow [1]
-
-nextRow :: [Integer] -> [Integer]
-nextRow lastRow = [1] ++ zipWith (+) lastRow (drop 1 lastRow) ++ [1]
+  where triangle = iterate nextRow [1]
+        nextRow lastRow = [1] ++ zipWith (+) lastRow (drop 1 lastRow) ++ [1]
