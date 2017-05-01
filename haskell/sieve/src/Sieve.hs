9d44fe11377be3f91500d0bbaa8b8491555507d5
Matthias Wimmer <m@tthias.eu>
2017-05-02 01:23:56 +0200

    4th iteration: Sieve @Haskell
    The lists are ordered. Use that fact and replace `Data.List.(\\)` with
    `Data.List.Ordered.minus`.

--- haskell/sieve/src/Sieve.hs
+++ haskell/sieve/src/Sieve.hs
@@ -1,8 +1,8 @@
 module Sieve (primesUpTo) where
 
-import Data.List ((\\))
+import Data.List.Ordered (minus)
 
 primesUpTo :: Integer -> [Integer]
 primesUpTo n = reverse $ sieve [] [2..n]
   where sieve primes [] = primes
-        sieve primes (p:ps) = sieve (p:primes) (ps \\ [2 * p, 3 * p .. n])
+        sieve primes (p:ps) = sieve (p:primes) (ps `minus` [2 * p, 3 * p .. n])
