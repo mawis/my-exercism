82d9b314cf7eb0af0bdf6230c6b5bed0b9f43421
Matthias Wimmer <m@tthias.eu>
2017-05-01 23:34:51 +0200

    2nd iteration: Sieve @Haskell
    Implement Sieve without `mod`.

--- haskell/sieve/src/Sieve.hs
+++ haskell/sieve/src/Sieve.hs
@@ -3,5 +3,5 @@ module Sieve (primesUpTo) where
 primesUpTo :: Integer -> [Integer]
 primesUpTo n = reverse $ sieve [] [2..n]
   where sieve primes [] = primes
-        sieve primes (x:xs) = sieve (x:primes) (filter (not . multiple) xs)
-          where multiple m = m `mod` x == 0
+        sieve primes (x:xs) = sieve (x:primes)
+                              (filter (not . (`elem` [x, 2 * x .. n])) xs)
