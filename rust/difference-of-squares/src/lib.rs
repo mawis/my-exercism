90ade5d5a1de6d875b74de8f34f3ddf84b06ca2e
Matthias Wimmer <m@tthias.eu>
2017-05-01 14:29:46 +0200

    2nd iteration: Difference of Squares @Rust
    * Using `sum()` instead of a `fold(…)`.

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn square_of_sum(limit: u32) -> u32 {
-    (1..limit + 1).fold(0, |accu, s| accu + s).pow(2)
+    (1..limit + 1).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(limit: u32) -> u32 {
-    (1..limit + 1).map(|n| n * n).fold(0, |accu, s| accu + s)
+    (1..limit + 1).map(|n| n * n).sum()
 }
 
 pub fn difference(limit: u32) -> u32 {
