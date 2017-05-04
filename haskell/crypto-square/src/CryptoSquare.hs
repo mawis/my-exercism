9f9f95a7071e29280af5e6ac502762c0674c35f7
Matthias Wimmer <m@tthias.eu>
2017-05-05 00:49:00 +0200

    2nd iteration: Crypto Square @Haskell
    * Using `chunksOf` from `Data.List.Split` instead of own `partition`
      function
    * Allow alphanumerics in general, not just in ASCII.

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -1,23 +1,12 @@
 module CryptoSquare (encode) where
 
-import Data.Char (isDigit, toLower)
+import Data.Char (isAlphaNum, toLower)
 import Data.List (transpose)
+import Data.List.Split (chunksOf)
 
 encode :: String -> String
-encode xs = unwords
-            . transpose
-            $ partition sqSize cleaned
-  where sqSize = ceiling doubleSqSize
+encode xs = unwords . transpose $ chunksOf sqSize cleaned
+  where cleaned = filter isAlphaNum . map toLower $ xs
+        sqSize = ceiling doubleSqSize
           where doubleSqSize :: Double
                 doubleSqSize = sqrt . fromIntegral $ length cleaned
-        cleaned = cleanString xs
-          where cleanString [] = []
-                cleanString (c:cs)
-                  | isDigit c                         = c:cleanString cs
-                  | lowercaseLetter `elem` ['a'..'z'] = lowercaseLetter
-                                                        :cleanString cs
-                  | otherwise                         = cleanString cs
-                  where lowercaseLetter = toLower c
-        partition len = takeWhile (not . null)
-                        . map (take len)
-                        . iterate (drop len)
