74d2d32e560e41d53f885e0848494643ec3c255e
Matthias Wimmer <m@tthias.eu>
2017-05-15 13:55:50 +0200

    2nd iteration: Wordy @Clojure
    Merged the RegEx to search for operation and number.

--- clojure/wordy/src/wordy.clj
+++ clojure/wordy/src/wordy.clj
@@ -1,31 +1,26 @@
 (ns wordy
   (:require [clojure.edn :as edn]))
 
-(defn number-rest [str]
-  (when-not (nil? str)
-    (when-let [match (re-matches #"(-?\d+)(\s+(.*))?" str)]
-      [(edn/read-string (get match 1)) (get match 3)])))
-
-(defn operation-rest [str]
+(defn operation-number-rest [str]
   (when-let [match
-             (re-matches #"(plus|minus|multiplied by|divided by)\s+(.*)" str)]
-    [(get match 1) (get match 2)]))
+             (re-matches
+              #"(plus|minus|multiplied by|divided by)\s+(-?\d+)(\s+(.*))?"
+              str)]
+    [(get match 1) (edn/read-string (get match 2)) (get match 4)]))
 
 (defn operations [str]
   (loop [ops []
          str str]
     (if (nil? str) ops
-        (let [[next-op rest] (operation-rest str)
-              [number rest'] (number-rest rest)]
+        (let [[next-op number rest] (operation-number-rest str)]
           (case next-op
-              "plus" (recur (conj ops #(+ % number)) rest')
-              "minus" (recur (conj ops #(- % number)) rest')
-              "multiplied by" (recur (conj ops #(* % number)) rest')
-              "divided by" (recur (conj ops #(/ % number)) rest'))))))
+              "plus" (recur (conj ops #(+ % number)) rest)
+              "minus" (recur (conj ops #(- % number)) rest)
+              "multiplied by" (recur (conj ops #(* % number)) rest)
+              "divided by" (recur (conj ops #(/ % number)) rest))))))
 
 (defn evaluate [exercise]
   (let [match (re-matches #"What is (-?\d+)\s+(.*)\?" exercise)]
-    (if match (reduce #(%2 %1)
-                      (edn/read-string (get match 1))
+    (if match (reduce #(%2 %1) (edn/read-string (get match 1))
                       (operations (get match 2)))
         (throw (IllegalArgumentException.)))))
