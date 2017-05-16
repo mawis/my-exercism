fd53035c85c5ceebec75c8862984e5cb705c25bc
Matthias Wimmer <m@tthias.eu>
2017-05-17 00:18:12 +0200

    2nd iteration: Secret Handshake @Java
    Code cleanup.

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -4,26 +4,27 @@ import java.util.function.UnaryOperator;
 
 import static com.google.common.collect.ImmutableList.*;
 import static com.google.common.collect.Iterables.concat;
-import static java.util.stream.IntStream.range;
+import static java.util.stream.IntStream.rangeClosed;
 
 final class HandshakeCalculator {
-	private static final
-		ImmutableMap<Integer, UnaryOperator<ImmutableList<Signal>>> ENCODINGS =
-		ImmutableMap.<Integer, UnaryOperator<ImmutableList<Signal>>>builder()
-		.put(0, accu -> copyOf(concat(accu, of(Signal.WINK))))
-		.put(1, accu -> copyOf(concat(accu, of(Signal.DOUBLE_BLINK))))
-		.put(2, accu -> copyOf(concat(accu, of(Signal.CLOSE_YOUR_EYES))))
-		.put(3, accu -> copyOf(concat(accu, of(Signal.JUMP))))
-		.put(4, ImmutableList::reverse)
-		.build();
+    private static final
+	ImmutableMap<Integer, UnaryOperator<ImmutableList<Signal>>> ENCODINGS =
+	ImmutableMap.of(0, append(Signal.WINK),
+			1, append(Signal.DOUBLE_BLINK),
+			2, append(Signal.CLOSE_YOUR_EYES),
+			3, append(Signal.JUMP),
+			4, ImmutableList::reverse);
 
-	public ImmutableList<Signal> calculateHandshake(final int number) {
-		return range(0, 5)
-			.filter(bit -> (number & (1 << bit)) != 0)
-			.mapToObj(ENCODINGS::get)
-			.reduce(
-					of(),
-					(accu, next) -> next.apply(accu),
-					(left, right) -> copyOf(concat(left, right)));
-	}
+    public ImmutableList<Signal> calculateHandshake(final int number) {
+	return rangeClosed(0, 4)
+	    .filter(bit -> (number & (1 << bit)) != 0)
+	    .mapToObj(ENCODINGS::get)
+	    .reduce(of(),
+		    (accu, next) -> next.apply(accu),
+		    (left, right) -> copyOf(concat(left, right)));
+    }
+
+    private static UnaryOperator<ImmutableList<Signal>> append(final Signal s) {
+	return list -> copyOf(concat(list, of(s)));
+    }
 }
