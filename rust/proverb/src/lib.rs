4b3416f861bf759794320461e2dd5244b1095706
Matthias Wimmer <m@tthias.eu>
2017-11-11 09:41:03 +0100

    2nd iteration: Proverb @Rust

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -4,8 +4,7 @@ pub fn build_proverb(list: Vec<&str>) -> String {
     match list.clone().first() {
         None => String::new(),
         Some(first) => {
-            interleaved_pairs(list)
-                .into_iter()
+            list.windows(2)
                 .map(format_pair)
                 .chain(once(format!("And all for the want of a {}.", first)))
                 .collect::<Vec<String>>()
@@ -14,20 +13,6 @@ pub fn build_proverb(list: Vec<&str>) -> String {
     }
 }
 
-fn interleaved_pairs(list: Vec<&str>) -> Vec<(&str, &str)> {
-    list.into_iter()
-        .fold((None, Vec::new()),
-              |(last, mut accu), next|
-              {
-                  match last {
-                      Some(name) => accu.push((name, next)),
-                      None => ()
-                  }
-                  (Some(next), accu)
-              }
-        ).1
-}
-
-fn format_pair((first, second): (&str, &str)) -> String {
-    format!("For want of a {} the {} was lost.", first, second)
+fn format_pair(pair: &[&str]) -> String {
+    format!("For want of a {} the {} was lost.", pair[0], pair[1])
 }
