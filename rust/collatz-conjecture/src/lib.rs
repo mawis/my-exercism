f024ed12050d4c3716576bc75786705ded4d4be8
Matthias Wimmer <m@tthias.eu>
2017-11-13 22:08:16 +0100

    1st iteration: Collatz Conjecture @Rust

---
+++ rust/collatz-conjecture/src/lib.rs
@@ -0,0 +1,20 @@
+extern crate itertools;
+use itertools::unfold;
+
+// return Ok(x) where x is the number of steps required to reach 1
+pub fn collatz(n: u64) -> Result<u64, &'static str> {
+    if n == 0 {
+        Err("n must not be zero!")
+    } else {
+        let numbers = unfold(n, next);
+        Ok(numbers.count() as u64)
+    }
+}
+
+fn next(n: &mut u64) -> Option<u64> {
+    if *n != 1 {
+        *n = if *n % 2 == 0 { *n / 2 } else { 3 * *n + 1 };
+        Some(*n)
+    }
+    else { None }
+}
