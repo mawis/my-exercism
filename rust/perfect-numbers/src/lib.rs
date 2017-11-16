86a19a044a83f10c01358126034e49b36689b2bf
Matthias Wimmer <m@tthias.eu>
2017-11-16 19:41:14 +0100

    1st iteration: Perfect Numbers @Rust

---
+++ rust/perfect-numbers/src/lib.rs
@@ -0,0 +1,21 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Classification {
+    Abundant,
+    Perfect,
+    Deficient
+}
+
+pub fn classify(num: u64) -> Result<Classification, & 'static str> {
+    match aliquot_sum(num) {
+        _ if num == 0 => Err("Number must be positive"),
+        n if n > num => Ok(Classification::Abundant),
+        n if n == num => Ok(Classification::Perfect),
+        _ => Ok(Classification::Deficient)
+    }
+}
+
+fn aliquot_sum(num: u64) -> u64 {
+    (1..num)
+        .filter(|n| num % n == 0)
+        .sum()
+}
