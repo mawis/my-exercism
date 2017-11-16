989c7606d1b3b4d3ef8cabca9b3e3e32d6e473d6
Matthias Wimmer <m@tthias.eu>
2017-11-16 19:50:37 +0100

    2nd iteration: Perfect Numbers @Rust
    * There can't be deviders in the second half of the interval from 1 to
      the checked number.

--- rust/perfect-numbers/src/lib.rs
+++ rust/perfect-numbers/src/lib.rs
@@ -15,7 +15,7 @@ pub fn classify(num: u64) -> Result<Classification, & 'static str> {
 }
 
 fn aliquot_sum(num: u64) -> u64 {
-    (1..num)
+    (1..(num / 2 + 1))
         .filter(|n| num % n == 0)
         .sum()
 }
