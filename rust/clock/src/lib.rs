c74eec5c3daa960b66351a74009bbe748ef72960
Matthias Wimmer <m@tthias.eu>
2017-11-16 20:32:54 +0100

    2nd iteration: Clock @Rust
    * We don't have to normalize the hours, we already normalized on
      creation
    * Minor code cleanup

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,20 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Clock {
     minutes_of_day: u32
 }
 
 impl Clock {
     pub fn new(h: i32, m: i32) -> Clock {
-        Clock { minutes_of_day: normalize(h * 60 + m) }
+        Clock::of_minutes(h * 60 + m)
     }
 
     pub fn add_minutes(&self, m: i32) -> Clock {
-        Clock { minutes_of_day: normalize(self.minutes_of_day as i32 + m) }
+        Clock::of_minutes(self.minutes_of_day as i32 + m)
+    }
+
+    pub fn of_minutes(m: i32) -> Clock {
+        Clock { minutes_of_day:
+                (m % (24 * 60) + if m < 0 { 24 * 60 } else { 0 }) as u32 }
     }
 
     pub fn to_string(&self) -> String {
@@ -17,20 +22,10 @@ impl Clock {
     }
 
     fn hour(&self) -> u32 {
-        self.minutes_of_day / 60 % 24
+        self.minutes_of_day / 60
     }
 
     fn minutes(&self) -> u32 {
         self.minutes_of_day % 60
     }
-}
-
-impl PartialEq for Clock {
-    fn eq(&self, other: &Clock) -> bool {
-        self.minutes_of_day == other.minutes_of_day
-    }
-}
-
-fn normalize(minute: i32) -> u32 {
-    (minute % (24 * 60) + if minute < 0 { 24 * 60 } else { 0 }) as u32
 }
