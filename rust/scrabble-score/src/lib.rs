059aa395906e07de48c67a140d2f58a661baf4c2
Matthias Wimmer <m@tthias.eu>
2017-11-19 02:13:51 +0100

    3rd iteration: Scrabble Score @Rust
    I just found the contains method on string can be used just with
    characters as an argument.

--- rust/scrabble-score/src/lib.rs
+++ rust/scrabble-score/src/lib.rs
@@ -2,8 +2,7 @@ static LETTER_SCORES: [&str; 10] =
     ["aeioulnrst", "dg", "bcmp", "fhvwy", "k", "", "", "jx", "", "qz"];
 
 pub fn score(word: &str) -> usize {
-    word.to_lowercase()
-        .chars()
+    word.to_lowercase().chars()
         .map(|ch| char_value(ch))
         .sum()
 }
@@ -11,8 +10,7 @@ pub fn score(word: &str) -> usize {
 fn char_value(ch: char) -> usize {
     LETTER_SCORES.iter()
         .zip(1..11)
-        .filter_map(|(chrs, score)|
-                    chrs.chars().filter(|&chr| chr == ch).nth(0).map(|_| score))
+        .filter_map(|(chrs, s)| if chrs.contains(ch) { Some(s) } else { None })
         .nth(0)
         .unwrap_or(0)
 }
