3f6d5bd016b50eb28b89475c74d71aabd45f568a
Matthias Wimmer <m@tthias.eu>
2017-11-19 02:00:55 +0100

    1st iteration: Scrabble Score @Rust

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,21 @@
+static LETTER_SCORES: [&str; 10] =
+    ["aeioulnrst", "dg", "bcmp", "fhvwy", "k", "", "", "jx", "", "qz"];
+
+pub fn score(word: &str) -> usize {
+    word.to_lowercase()
+        .chars()
+        .map(|ch| char_value(ch))
+        .sum()
+}
+
+fn char_value(ch: char) -> usize {
+    LETTER_SCORES.iter()
+        .zip(1..11)
+        .filter_map(|(chrs, score)|
+                    match chrs.chars().filter(|&chr| chr == ch).nth(0) {
+                        None => None,
+                        Some(_) => Some(score)
+                    })
+        .nth(0)
+        .unwrap_or(0)
+}
