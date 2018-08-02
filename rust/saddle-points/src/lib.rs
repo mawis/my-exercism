23755d9e4788caa7cb8a8cc092220d94762e92c7
Matthias Wimmer <m@tthias.eu>
2018-08-02 23:15:15 +0200

    2nd iteration: Saddle Points @Rust
    Thanks to my mentor @ptillemans for his support.

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -3,25 +3,19 @@ use std::u64;
 
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     matrix_indices(input)
-        .iter()
-        .cloned()
         .filter(|(row, col)| test_saddle_point(input, *row, *col))
         .collect()
 }
 
-fn matrix_indices(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    (0..input.len())
-        .flat_map(|row|
-                  (0..input[row].len())
-                  .map(move |col| (row, col)))
-        .collect()
+fn matrix_indices<'a>(input: &'a [Vec<u64>])
+                      -> impl Iterator<Item=(usize,usize)> + 'a {
+    (0..input.len()).flat_map(move |row|
+                              (0..input[row].len()).map(move |col| (row, col)))
 }
 
 fn test_saddle_point(input: &[Vec<u64>], row: usize, col: usize) -> bool {
-    let r = input[row].iter();
-    let max_in_row = r.fold(0, |a, &b| if a > b { a } else { b });
-    let c = input.iter().map(|r| r[col]);
-    let min_in_col = c.fold(u64::MAX, cmp::min);
+    let max_in_row = input[row].iter().cloned().fold(0, cmp::max);
+    let min_in_col = input.iter().map(|r| r[col]).fold(u64::MAX, cmp::min);
 
     max_in_row == min_in_col && input[row][col] == max_in_row
 }
