2f77bd232cc2d14a6a2ad825fd3ddd6b5bc27fb7
Matthias Wimmer <m@tthias.eu>
2018-08-04 00:48:52 +0200

    2nd iteration: Atbash Cipher @Rust
    Don't group and remove groups again in the case of decoding, but only
    create the character groups when needed.

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -2,15 +2,7 @@ use std::char;
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    plain.chars()
-        .filter(|ch| ch.is_alphanumeric() && ch.is_ascii())
-        .flat_map(char::to_lowercase)
-        .flat_map(|ch|
-                  if ch.is_ascii() && ch.is_alphabetic() {
-                      char::from_u32('z' as u32 - (ch as u32 - 'a' as u32))
-                  } else {
-                      Some(ch)
-                  })
+    decode(plain).chars()
         .enumerate()
         .flat_map(|(pos, ch)|
                   if pos > 0 && pos % 5 == 0 { vec![' ', ch] }
@@ -20,7 +12,14 @@ pub fn encode(plain: &str) -> String {
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    encode(cipher).chars()
-        .filter(|ch| ch.is_alphanumeric())
+    cipher.chars()
+        .filter(|ch| ch.is_alphanumeric() && ch.is_ascii())
+        .flat_map(char::to_lowercase)
+        .flat_map(|ch|
+                  if ch.is_ascii() && ch.is_alphabetic() {
+                      char::from_u32('z' as u32 - (ch as u32 - 'a' as u32))
+                  } else {
+                      Some(ch)
+                  })
         .collect()
 }
