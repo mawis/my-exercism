6399de5ffac2e64095e22837bb955a197a750aa3
Matthias Wimmer <m@tthias.eu>
2018-08-03 11:05:59 +0200

    3rd iteration: Saddle Points @Rust
    Improved readability.

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,6 +1,3 @@
-use std::cmp;
-use std::u64;
-
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     matrix_indices(input)
         .filter(|(row, col)| test_saddle_point(input, *row, *col))
@@ -14,8 +11,8 @@ fn matrix_indices<'a>(input: &'a [Vec<u64>])
 }
 
 fn test_saddle_point(input: &[Vec<u64>], row: usize, col: usize) -> bool {
-    let max_in_row = input[row].iter().cloned().fold(0, cmp::max);
-    let min_in_col = input.iter().map(|r| r[col]).fold(u64::MAX, cmp::min);
+    let max_in_row = input[row].iter().cloned().max().unwrap();
+    let min_in_col = input.iter().map(|r| r[col]).min().unwrap();
 
-    max_in_row == min_in_col && input[row][col] == max_in_row
+    input[row][col] == min_in_col && input[row][col] == max_in_row
 }
