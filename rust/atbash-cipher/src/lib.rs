26c843aa26cb76d8dbe3af8b513453214bc5be7b
Matthias Wimmer <m@tthias.eu>
2018-08-04 19:49:51 +0200

    4th iteration: Atbash Cipher @Rust

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,5 +1,7 @@
 use std::char;
 
+const ALPHABETIC_MIRROR_POSITION: u32 = 'z' as u32 + 'a' as u32;
+
 pub fn encode(plain: &str) -> String {
     atbash_transpose(plain)
         .enumerate()
@@ -16,12 +18,19 @@ pub fn decode(cipher: &str) -> String {
 
 fn atbash_transpose<'a>(text: &'a str) -> impl Iterator<Item = char> + 'a {
     text.chars()
-        .filter(|ch| ch.is_alphanumeric() && ch.is_ascii())
+        .filter(codeable_character)
         .flat_map(char::to_lowercase)
-        .flat_map(|ch|
-                  if ch.is_ascii() && ch.is_alphabetic() {
-                      char::from_u32('z' as u32 - (ch as u32 - 'a' as u32))
-                  } else {
-                      Some(ch)
-                  })
+        .map(transpose_alphabetics)
+}
+
+fn codeable_character(ch: &char) -> bool {
+    ch.is_alphanumeric() && ch.is_ascii()
+}
+
+fn transpose_alphabetics(ch: char) -> char {
+    if ch.is_ascii() && ch.is_alphabetic() {
+        char::from_u32(ALPHABETIC_MIRROR_POSITION - (ch as u32)).unwrap()
+    } else {
+        ch
+    }
 }
